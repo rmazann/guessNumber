8b9a28d898eb9dbf9d7b6929caa869866c38384e
rmazann <20182305014@stu.khas.edu.tr>
2025-01-28 01:15:24 +0300

    Initial Commit

---
+++ guessNumber/guessNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  guessNumberApp.swift
+//  guessNumber
+//
+//  Created by Ramazan Tutar on 28.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct guessNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ guessNumberTests/guessNumberTests.swift
@@ -0,0 +1,17 @@
+//
+//  guessNumberTests.swift
+//  guessNumberTests
+//
+//  Created by Ramazan Tutar on 28.01.2025.
+//
+
+import Testing
+@testable import guessNumber
+
+struct guessNumberTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
